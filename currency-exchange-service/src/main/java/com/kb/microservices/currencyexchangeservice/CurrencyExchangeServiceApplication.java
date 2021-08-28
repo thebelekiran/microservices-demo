9bd25e7b521c1ac6e38574fea05f5e201a73cfb6
thebelekiran <belekiran@live.com>
2021-08-28 19:50:38 +0530

    Naming Server added.
    for Ribbon removing multiple load balancing URL hardcoded to properties file in exchange service, and are removed by use of Netflix eureka naming server.

--- currency-exchange-service/src/main/java/com/kb/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/kb/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,11 +3,13 @@ package com.kb.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "com.kb.microservices.currencyexchangeservice.model" })
 @EnableJpaRepositories(basePackages = { "com.kb.microservices.currencyexchangeservice.repository" })
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
---
+++ netflix-eureka-naming-server/src/test/java/com/kb/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
